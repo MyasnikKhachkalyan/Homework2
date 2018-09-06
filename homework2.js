12e368dae45d1249b8cff84a23b95735c2f0e43d
unknown <khachkalyan.myasnik@gmail.com>
2018-09-06 20:44:36 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,38 @@
+const a = 2;
+const b =16;
+const c = 'barev @nker Rouben :)'
+const d = true;
+const e = 1;
+const f = 0;
+
+alert(c);
+
+const s = a+b;
+console.log(s);
+
+const p = a*b;
+console.log(p);
+
+const t = b%a;
+
+if (t===0){
+	console.log(b + ' is an even number');
+ }
+
+ if (d!==false && d) {
+ 	console.log("esel mer boolean logicy :) ");
+ }
+ if ((a*b-1)%2===e) {
+ 	console.log("yey we did it!!!");
+ }
+ else if ((a*b-1)%2===d) {
+ 	console.log("I think smth is wrong");
+ }
+
+const m = 8;
+
+if ((b/a===m || (b-a)===e) && d === !!!!!!false ) {
+	console.log("good one");
+} else {
+	console.log('anything can happen');
+}
